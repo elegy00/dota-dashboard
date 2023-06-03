94d57e4b70232276c85ce9b9299c89724859e3e9
christoph <christoph.salvisberg@gmail.com>
2023-06-03 14:49:12 +0200

    Remove border

--- app/components/organism/Layout/Layout.css.ts
+++ app/components/organism/Layout/Layout.css.ts
@@ -4,7 +4,6 @@ import { theme } from "~/styles/theme/colors.css";
 import { sizes } from "~/styles/theme/sizes.css";
 
 export const root = style({
-  border: "solid 1px",
   background: theme.grey[100],
   color: theme.grey[900],
   fontSize: 16,
--- app/styles/tournament.css.ts
+++ app/styles/tournament.css.ts
@@ -2,7 +2,6 @@ import { style } from "@vanilla-extract/css";
 import { theme } from "./theme/colors.css";
 
 export const root = style({
-  border: "solid 1px",
   background: theme.grey[100],
   color: theme.grey[900],
 });

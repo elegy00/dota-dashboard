8bc01f376ec53f77f08c69f977d2cf4583bde011
christoph <christoph.salvisberg@gmail.com>
2023-06-03 14:25:55 +0200

    Some basic layout with sizing and breakpoints

--- app/components/organism/Layout/Layout.css.ts
+++ app/components/organism/Layout/Layout.css.ts
@@ -1,9 +1,20 @@
 import { style } from "@vanilla-extract/css";
+import { breakpoints } from "~/styles/theme/breakpoints.css";
 import { theme } from "~/styles/theme/colors.css";
+import { sizes } from "~/styles/theme/sizes.css";
 
 export const root = style({
   border: "solid 1px",
   background: theme.grey[100],
   color: theme.grey[900],
   fontSize: 16,
+  padding: sizes["04"],
+  "@media": {
+    [breakpoints.sm]: {
+      padding: [sizes["06"]],
+    },
+    [breakpoints.lg]: {
+      padding: [sizes["08"]],
+    },
+  },
 });

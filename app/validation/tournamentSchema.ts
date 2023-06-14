d8d73402fdfaec5293a3b727b429fa7d7640b36f
christoph <christoph.salvisberg@gmail.com>
2023-06-14 22:59:32 +0200

    Make Schema strict

--- app/validation/tournamentSchema.ts
+++ app/validation/tournamentSchema.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
-export const tournamentSchema = z.object({
-  name: z.string().nonempty(),
-  description: z.string().optional(),
-});
+export const tournamentSchema = z
+  .object({
+    name: z.string().nonempty(),
+    description: z.string().optional(),
+  })
+  .strict();
 
 export type TournamentFm = z.infer<typeof tournamentSchema>;

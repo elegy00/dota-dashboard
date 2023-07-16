60def4ad1cce6367b047610ab6f4ab75ea57cd62
christoph <christoph.salvisberg@gmail.com>
2023-07-16 11:42:00 +0200

    use hero image from steam (also includes dawn)

--- app/transformation/heroToHeroImage.ts
+++ app/transformation/heroToHeroImage.ts
@@ -2,15 +2,10 @@ import heroesData from "dotaconstants/build/heroes.json";
 import type { HeroImage } from "~/types/aggregation";
 
 const heroToHeroImage = (hero_id: number): HeroImage => {
-  // const heroIds = match?.players.map((p) => p.hero_id);
-
-  var hero = Object.values(heroesData)
-    // .filter((hd) => heroIds?.includes(hd.id))
-    .find((hd) => hero_id === hd.id);
+  var hero = Object.values(heroesData).find((hd) => hero_id === hd.id);
 
   const name = hero?.name.substring("npc_dota_hero_".length) ?? "";
-  const heroImageUrl = `https://cdn.dota2.com/apps/dota2/images/heroes/${name}_full.png`;
-
+  const heroImageUrl = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${name}.png`;
   return { heroImageUrl, name };
 };
 

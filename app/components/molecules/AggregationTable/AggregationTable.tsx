af02efaac66bf7dea00f545f7c59a0161ac6b94c
christoph <christoph.salvisberg@gmail.com>
2023-07-16 11:25:30 +0200

    Some aggregation layouting

--- app/components/molecules/AggregationTable/AggregationTable.tsx
+++ app/components/molecules/AggregationTable/AggregationTable.tsx
@@ -1,5 +1,6 @@
 import type { Aggregation } from "~/types/aggregation";
 import { AggregationEntryRow } from "./AggregationEntryRow";
+import { categoryBorder, valueBorder } from "./styles";
 
 interface Props {
   aggregation: Aggregation;
@@ -8,23 +9,51 @@ interface Props {
 const AggregationTable: React.FC<Props> = (props) => {
   const { aggregation } = props;
 
+  if (aggregation.entries.length === 0) {
+    return <>No Data</>;
+  }
+
+  const hasHeroImage = aggregation.entries[0].hero ? 1 : 0;
   const columnCount =
     aggregation.entries[0].categories.reduce<number>(
       (sum, cat) => sum + cat.valueGroups.length,
       0
     ) +
-    (aggregation.entries[0].hero ? 1 : 0) +
+    (hasHeroImage ? 1 : 0) +
     1;
 
   return (
     <div
-      className="grid gap-2"
+      className="grid child:p-2"
       key={aggregation?.id}
       style={{
         gridTemplateColumns: `repeat(${columnCount},minmax(4rem,8rem)`,
       }}
     >
-      {aggregation?.entries.map((entry, index) => (
+      <div style={{ gridColumn: `span ${hasHeroImage ? 2 : 1}` }}></div>
+      {aggregation.entries[0].categories.map((cat) => (
+        <div
+          key={cat.id}
+          style={{ gridColumn: `span ${cat.valueGroups.length}` }}
+          className={categoryBorder}
+        >
+          {cat.label}
+        </div>
+      ))}
+      <div style={{ gridColumn: `span ${hasHeroImage ? 2 : 1}` }}></div>
+      {aggregation.entries[0].categories.map((cat) =>
+        cat.valueGroups.map((vg, index) => (
+          <div
+            key={vg.id}
+            className={
+              index < cat.valueGroups.length - 1 ? valueBorder : categoryBorder
+            }
+          >
+            {vg.label}
+          </div>
+        ))
+      )}
+      {aggregation?.entries.map((entry) => (
         <AggregationEntryRow entry={entry} key={entry.id} />
       ))}
     </div>

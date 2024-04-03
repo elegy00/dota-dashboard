deef5e3ec2fda90c87c6f3a6e3ad262ce4ffe4fc
christoph <christoph.salvisberg@gmail.com>
2024-04-03 19:52:42 +0200

    Move old app and recreate

--- app/root.tsx
+++ app/root.tsx
@@ -1,56 +1,29 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-// import {
-//   darkColors,
-//   lightColors,
-//   theme,
-//   themeClass,
-// } from "./styles/theme/colors.css";
-import { ColorContext, useColorThemeState } from "./state/colorContext";
-import { Layout } from "./components/organism/Layout";
 
-import styles from "./tailwind.css";
-
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
-
-export default function App() {
-  const contextState = useColorThemeState();
-
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500&display=swap"
-          rel="stylesheet"
-        ></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <ColorContext.Provider value={contextState}>
-        <body
-          className={`${
-            contextState.theme === "light" ? "light-mode" : "dark-mode"
-          } bg-grey-100 text-grey-900`}
-          // style={{ backgroundColor: theme.grey[100] }}
-        >
-          <Layout>
-            <Outlet />
-            <ScrollRestoration />
-            <Scripts />
-            <LiveReload />
-          </Layout>
-        </body>
-      </ColorContext.Provider>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Outlet />;
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,39 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-import { Link } from "~/components/atoms/Link";
-import { pageTitle } from "~/util/meta";
+import type { MetaFunction } from "@remix-run/node";
 
-export const meta: V2_MetaFunction = () => {
-  return [{ title: pageTitle("home") }];
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
 };
 
 export default function Index() {
   return (
-    <div>
-      <h1>Welcome to the dota2 dashboard</h1>
-      <p>Browse some tournaments to view details</p>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <Link to="/tournament">Tournament</Link>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
         </li>
       </ul>
     </div>
